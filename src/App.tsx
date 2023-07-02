e8aae6fb96ffbc54f127f8b184f32693a22c7bef
VictorMFL <vm859222@gmail.com>
2023-07-01 23:15:28 -0300

    add NavBar

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
+import { useState } from "react";
+
+import NavBar from "./components/NavBar"
+
 const App = () => {
+  const [active, setActive] = useState<number>(1);
   return (
-    <div>
-      <h1>Teste</h1>
+    <div className="container">
+      <NavBar active={active} />
     </div>
   )
 }
---
+++ src/components/NavBar.tsx
@@ -0,0 +1,57 @@
+import "../styles/components/NavBar.sass";
+
+import { AiOutlineRight } from "react-icons/ai";
+import { BsFillCheckCircleFill } from "react-icons/bs";
+
+type NavBarProps = {
+  active: number;
+};
+
+const NavBar = ({ active }: NavBarProps) => {
+  return (
+    <header>
+      <nav>
+        <section>
+          {active === 2 || active == 3 ? (
+            <BsFillCheckCircleFill size={28} className="icon" />
+          ) : (
+            <div
+              className={active === 1 ? "active_number" : "container_number"}
+            >
+              <p>1</p>
+            </div>
+          )}
+          <p
+            className={
+              active === 1 || active === 2 || active === 3 ? "active_title" : ""
+            }
+          >
+            Contato
+          </p>
+        </section>
+        <AiOutlineRight size={24} />
+        <section>
+          {active === 3 ? (
+            <BsFillCheckCircleFill size={28} className="icon" />
+          ) : (
+            <div
+              className={active === 2 ? "active_number" : "container_number"}
+            >
+              <p>2</p>
+            </div>
+          )}
+          <p className={active === 2 || active === 3 ? "active_title" : ""}>Empresa</p>
+        </section>
+        <AiOutlineRight size={24} />
+        <section>
+          <div className={active === 3 ? "active_number" : "container_number"}>
+            <p>3</p>
+          </div>
+          <p className={active === 3 ? "active_title" : ""}>Projeto</p>
+        </section>
+      </nav>
+    </header>
+  );
+};
+
+export default NavBar;
